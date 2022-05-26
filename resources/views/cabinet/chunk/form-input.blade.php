69eff851b35a79ac1524774e44b6843403346fc3
Andrey Lygun <lygun@yandex.ru>
2022-05-26 22:14:58 +0300

    something

--- resources/views/cabinet/chunk/form-input.blade.php
+++ resources/views/cabinet/chunk/form-input.blade.php
@@ -1,13 +1,29 @@
-<div class="row  {{isset($class)?$class:''}}">
-    <div class="col-12 col-md-4">
-        <label for="{{$key}}" class="form-label mt-1 mb-0">{{$label}}</label>
+@if(isset($inrow))
+    <div class="row {{isset($class)?$class:''}}">
+        <div class="col-12 col-md-4">
+            <label for="{{$key}}" class="form-label mt-1 mb-0">{{isset($label)?$label:''}}</label>
+        </div>
+        <div class="col-12 col-md-8">
+            <input type="{{$type}}" class="form-control"  id="{{$key}}" name="{{$key}}" value="{{$value}}" {{isset($attributes)?$attributes:''}}>
+            @error($key)
+                <div class="form-text text-danger">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
     </div>
-    <div class="col-12 col-md-8">
-        <input type="{{$type}}" class="form-control"  id="{{$key}}" name="{{$key}}" value="{{$value}}">
+@else
+    <div class="my-3">
+        @isset($label)
+            <label for="{{$key}}" class="form-label">{{$label}}</label>
+        @endisset
+        <input type="{{$type}}" class="form-control" id="{{$key}}" placeholder="{{isset($placeholder)?$placeholder:''}}" value="{{$value}}" {{isset($attributes)?$attributes:''}}>
         @error($key)
             <div class="form-text text-danger">
                 {{$message}}
             </div>
         @enderror
     </div>
-</div>
+@endif
+
+
